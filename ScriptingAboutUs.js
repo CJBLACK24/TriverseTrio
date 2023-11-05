b8297cf9f346a375d40476c227fd67068d3aa57b
DUQUE CHRISTIAN JOHN C <117024837+CJBLACK24@users.noreply.github.com>
2023-11-05 16:11:49 +0800

    Add files via upload

---
+++ ScriptingAboutUs.js
@@ -0,0 +1,67 @@
+ ScrollReveal({
+      reset: true,
+      distance: '80px', 
+      duration: 2000,
+      delay:200
+     });
+
+
+     ScrollReveal().reveal('.about-image', { origin: 'left'});
+     ScrollReveal().reveal('.about-content p', { origin: 'right'});
+     ScrollReveal().reveal('.about-content h2', { origin: 'left'});
+
+
+
+
+     ScrollReveal().reveal('.Christian, .heading', { origin: 'top'});
+     ScrollReveal().reveal('.SHESHHHH, .heading', { origin: 'bottom'});
+     ScrollReveal().reveal('.Christian h1', { origin: 'left'});
+     ScrollReveal().reveal('.Christian p', { origin: 'right'});
+     ScrollReveal().reveal('.social-media', { origin: 'bottom'});
+
+ 
+
+
+
+
+    ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+    ScrollReveal().reveal('.Pogi-Problems, .heading', { origin: 'top'});
+    ScrollReveal().reveal('.home-content h1', { origin: 'right'});
+    ScrollReveal().reveal('.home-content p', { origin: 'left'});
+    ScrollReveal().reveal('.home-sci', { origin: 'bottom'});
+  
+    
+
+  const Christian = new Typed('.multiple-text',{
+    strings: ['Front-End Developer','Software Developer','Cyber Security'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+  });
+
+  const LeoJohn = new Typed('.text',{
+    strings: ['UI Designer','UX Designer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+  });
+
+  const Denielle = new Typed('.BasicByEmmanne',{
+    strings: ['UI Designer','UX Designer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+  });
+
+  const Okay = new Typed('.TEXTONE',{
+    strings: ['Craft'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+  });
+
+ 
